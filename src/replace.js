36e532821aa58e635fad1a9b323647c71e6cfde7
abhinavminhas <abhinavminhas@users.noreply.github.com>
2023-07-16 17:01:32 +1000

    Remove any spaces from delimiter input value

--- src/replace.js
+++ src/replace.js
@@ -5,6 +5,8 @@ const fs = require('fs')
 async function replaceTokens (files, replacements, delimiter = ',') {
   if (delimiter === null || delimiter === undefined || delimiter.length <= 0) {
     delimiter = ','
+  } else {
+    delimiter = delimiter.replace(/\s/g, '')
   }
   const fileNames = files.replace(/\s/g, '').split(delimiter)
   const replacementValues = replacements.replace(/\s/g, '').split(delimiter)
